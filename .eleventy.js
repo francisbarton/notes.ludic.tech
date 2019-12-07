671b42aef5b84e627ba988c0725bfbdeece66fc3
Francis Barton <franbarton@gmail.com>
2019-12-07 23:55:43 +0000

    Add footer.njk as footer template. Install markdown-shortcode plugin.

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,14 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const markdownShortcode = require("eleventy-plugin-markdown-shortcode");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight, {
     templateFormats: ["*"]
   });
+  eleventyConfig.addPlugin(markdownShortcode);
   eleventyConfig.addPlugin(pluginNavigation);
 
   eleventyConfig.setDataDeepMerge(true);
